f3c492795f8e3f5203cb24501bdcc80b0ac4fa85
Nicolas Barcacci <nbarcacci@swood.fr>
2024-04-30 16:06:10 +0200

    improve log

--- app/src/main/java/com/accor/testviewmodellifecycleinnavgraph/home/HomeViewModel.kt
+++ app/src/main/java/com/accor/testviewmodellifecycleinnavgraph/home/HomeViewModel.kt
@@ -3,14 +3,20 @@ package com.accor.testviewmodellifecycleinnavgraph.home
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
+private var GlobalIndex = 0
+
 class HomeViewModel : ViewModel() {
 
+    private val index: Int
+
     init {
-        Log.d("MYTAG", "HomeViewModel - init")
+        GlobalIndex++
+        index = GlobalIndex
+        Log.d("MYTAG", "HomeViewModel - init ($index)")
     }
 
     override fun onCleared() {
-        Log.d("MYTAG", "HomeViewModel - onCleared")
+        Log.d("MYTAG", "HomeViewModel - onCleared ($index)")
         super.onCleared()
     }
 }
--- app/src/main/java/com/accor/testviewmodellifecycleinnavgraph/search/SearchViewModel.kt
+++ app/src/main/java/com/accor/testviewmodellifecycleinnavgraph/search/SearchViewModel.kt
@@ -3,14 +3,20 @@ package com.accor.testviewmodellifecycleinnavgraph.search
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
+private var GlobalIndex = 0
+
 class SearchViewModel : ViewModel() {
 
+    private val index: Int
+
     init {
-        Log.d("MYTAG", "SearchViewModel - init")
+        GlobalIndex++
+        index = GlobalIndex
+        Log.d("MYTAG", "SearchViewModel - init ($index)")
     }
 
     override fun onCleared() {
-        Log.d("MYTAG", "SearchViewModel - onCleared")
+        Log.d("MYTAG", "SearchViewModel - onCleared ($index)")
         super.onCleared()
     }
 }
